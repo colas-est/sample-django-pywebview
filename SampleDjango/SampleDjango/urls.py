732062917dfd697ed32eff1dac4b998cdfba8e51
Alexandre <a-palo@laposte.net>
2017-01-30 11:15:23 +0100

    first django app + sample

---
+++ SampleDjango/SampleDjango/urls.py
@@ -0,0 +1,25 @@
+"""SampleDjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from main.views import ListIntervention, DoneIntervention
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^interventions/$', ListIntervention.as_view(), name='url_list_intervention'),
+    url(r'^interventions/done/$', DoneIntervention.as_view(), name='url_done_intervention')
+]
---
+++ SampleDjango/main/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+from django.views.generic import FormView
+from django import forms
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(
+        label='Recherche',
+        required=False,
+        max_length=254
+    )
+    principal_organ = forms.ChoiceField(
+        label='Organe principal',
+        required=False,
+        choices=(
+            (None, 'Selectionner'),
+            ('reglementary', 'Contrôle réglementaire'),
+            ('tambour', 'Tambour sêcheur'),
+            ('predoseur', 'Prédoseurs'),
+            ('bandes', 'Bandes transporteuses'),
+            ('tour', 'Tour')
+        ),
+        widget=forms.Select(attrs={'class': 'selectpicker'})
+    )
+
+
+class DoneInterventionForm(forms.Form):
+    date = forms.CharField(
+        label='Date de réalisation',
+        widget=forms.TextInput(attrs={'class': 'datepicker'})
+    )
+    remark = forms.CharField(
+        label='Remarque',
+        widget=forms.Textarea
+    )
+
+
+class ListIntervention(FormView):
+    template_name='list_intervention.html'
+    form_class = SearchForm
+    success_url = '/interventions/'
+
+
+class DoneIntervention(FormView):
+    template_name='done_intervention.html'
+    form_class = DoneInterventionForm
+    success_url = '/interventions/'
